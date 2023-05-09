baa3186723fb78a52b3e4864bef772d70aa21424
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-05-10 00:37:55 +0530

    add index.html

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from argparse import Namespace
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name ='index.html'), name="home"),
     path('admin/', admin.site.urls),
     path('blog/', include(('blog.urls', 'blog'),namespace='blog')),
 ]
