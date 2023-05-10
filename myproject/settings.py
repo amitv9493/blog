c52aedf15ffcfe39031ec143877d8061a370d165
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-05-10 15:22:21 +0530

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -18,7 +18,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['3.136.145.233',
                  '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['http://3.136.145.233']  
+CSRF_TRUSTED_ORIGINS = ['https://3.136.145.233']  
 
 # Application definition
 
