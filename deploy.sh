4248fd3243e0be26b20bc688d7c8b30428167aa3
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-05-12 23:38:24 +0530

    minor change

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh     
-sudo git pull origin master
+sudo git pull origin main
+source ../myprojectenv/bin/activate
 sudo pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
